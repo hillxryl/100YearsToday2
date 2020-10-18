44eba6f69d1cb399727b8d20c46cab58553d1fc5
hillary.leung@hotmail.com <hillxryl@users.noreply.github.com>
2020-10-17 20:01:22 -0400

    initial

---
+++ Unity-ARFoundation-echoAR-example-master/Unity-ARFoundation-echoAR-example-master/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public GameObject FutureCamPanel, DailyLogPanel, EcoHabitsPanel, MissionsPanel;
+
+    void Start()
+    {
+        DailyLogPanel.SetActive(false);
+    }
+
+    public void OpenDailyLog()
+    {
+        DailyLogPanel.SetActive(true);
+        FutureCamPanel.SetActive(false);
+    }
+}
